babf50f6daca16821186f88ea056225d6e08fdc7
Mariam Anuashvili <m.anuashvili@gmail.com>
2019-12-17 21:19:43 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,10 +1,15 @@
+#def find_element_index(array, value_to_find)
+  #array.length.times do |count|
+    #if array[count] == value_to_find
+      #return count
+    #end
+  #end
+  #nil
+#end
+
+#Another way
 def find_element_index(array, value_to_find)
-  array.length.times do |count|
-    if array[count] == value_to_find
-      return count
-    end
-  end
-  nil
+  array.index(value_to_find)
 end
 
 def find_max_value(array)
