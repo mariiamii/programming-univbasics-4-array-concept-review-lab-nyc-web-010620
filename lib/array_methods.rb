2e069288bf8a67c2d4e9debdc4a2094b50cf89e7
Mariam Anuashvili <m.anuashvili@gmail.com>
2019-12-17 20:17:37 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,10 +1,8 @@
 def find_element_index(array, value_to_find)
-  do
-    
-  end
-  do
-    puts nil 
+  array.length.times do |index|
+    puts index
   end 
+  nil 
 end
 
 def find_max_value(array)
