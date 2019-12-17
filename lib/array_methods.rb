ee72ffd1efe84abc5b4a622bb49c23f986d5c415
Mariam Anuashvili <m.anuashvili@gmail.com>
2019-12-17 21:06:22 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,6 @@
 def find_element_index(array, value_to_find)
-  array.length.times do |index|
-  if array[index] == value_to_find
+  array.length.times do |count|
+  if array[count] == value_to_find
     return count
   end 
   nil 
